83f247cbfb9c76a23e715c6dc6499de78a435a51
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:42 -0500

    TUnit conversion

--- test/Soenneker.Dtos.Users.Identity.Tests/UserIdentityTests.cs
+++ test/Soenneker.Dtos.Users.Identity.Tests/UserIdentityTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Dtos.Users.Identity.Tests;
 
 public sealed class UserIdentityTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
